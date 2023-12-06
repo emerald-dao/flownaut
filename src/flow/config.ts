53e2057a69d34ee108eac912d52914e977575689
jacob-tucker <jacobtucker818@gmail.com>
2023-12-05 22:37:55 -0500

    beef up config

--- src/flow/config.ts
+++ src/flow/config.ts
@@ -1,20 +1,25 @@
 import { config } from '@onflow/fcl';
 import dappInfo from '$lib/config/config';
 
-// EMULATOR
-// export const network = 'emulator';
-// const accessNode = 'http://127.0.0.1:8888';
-// const discoveryWallet = 'http://localhost:8701/fcl/authn';
-
-// TESTNET
 export const network = 'testnet';
-const accessNode = 'https://rest-testnet.onflow.org';
-const discoveryWallet = 'https://fcl-discovery.onflow.org/testnet/authn';
 
-// MAINNET
-// export const network = 'mainnet';
-// const accessNode = 'https://rest-mainnet.onflow.org';
-// const discoveryWallet = 'https://fcl-discovery.onflow.org/authn';
+const fclConfigInfo = {
+	emulator: {
+		accessNode: 'http://127.0.0.1:8888',
+		discoveryWallet: 'http://localhost:8701/fcl/authn',
+		discoveryAuthInclude: []
+	},
+	testnet: {
+		accessNode: 'https://rest-testnet.onflow.org',
+		discoveryWallet: 'https://fcl-discovery.onflow.org/testnet/authn',
+		discoveryAuthnEndpoint: 'https://fcl-discovery.onflow.org/api/testnet/authn'
+	},
+	mainnet: {
+		accessNode: 'https://rest-mainnet.onflow.org',
+		discoveryWallet: 'https://fcl-discovery.onflow.org/authn',
+		discoveryAuthnEndpoint: 'https://fcl-discovery.onflow.org/api/authn'
+	}
+};
 
 const resolver = async () => {
 	const nonce = '7f190deedcd3b0538b7cd0ebc1994ed40d9db16cc1a6fcc3e7a994240c14d86d';
@@ -28,6 +33,7 @@ config()
 	.put('app.detail.title', dappInfo.title)
 	.put('app.detail.icon', dappInfo.icon)
 	.put('fcl.accountProof.resolver', resolver)
-	.put('accessNode.api', accessNode)
+	.put('accessNode.api', fclConfigInfo[network].accessNode)
 	.put('flow.network', network)
-	.put('discovery.wallet', discoveryWallet);
+	.put('discovery.wallet', fclConfigInfo[network].discoveryWallet)
+	.put('discovery.authn.endpoint', fclConfigInfo[network].discoveryAuthnEndpoint);
