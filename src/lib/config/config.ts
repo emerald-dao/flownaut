9d1d370eaa3676adb53401bba070d7876093ea00
jacob-tucker <jacobtucker818@gmail.com>
2023-12-05 22:42:29 -0500

    update dappinfo description

--- src/lib/config/config.ts
+++ src/lib/config/config.ts
@@ -1,6 +1,6 @@
 const dappInfo = {
 	title: 'Flownaut',
-	description: 'A Web3/Cadence based wargame.',
+	description: 'A Cadence hacking game inspired by Ethernaut.',
 	url: 'https://flownaut.ecdao.org',
 	author: 'Emerald City DAO',
 	icon: 'https://i.imgur.com/XxjD3wF.png'
